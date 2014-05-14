36e2edbe1ec8e86722ed51ce990ce0a24d747320
noproblembabe <marcelosfm@gmail.com>
2014-05-14 12:34:52 -0300

    Finished the first set of tests - substitution

--- Surrogates.Tests/Substitute/FuncWithFuncTest.cs
+++ Surrogates.Tests/Substitute/FuncWithFuncTest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DynamicProxy;
+using NUnit.Framework;
+using Surrogates.Tests.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,26 +9,132 @@ using System.Threading.Tasks;
 
 namespace Surrogates.Tests.Substitute
 {
-    class FuncWithFuncTest: IInterferenceTest
+    [TestFixture]
+    public class FuncWithFuncTest: IInterferenceTest
     {
+        [Test]
         public void BothParameterLess()
         {
-            throw new NotImplementedException();
+            var container = new SurrogatesContainer();
+
+            container.Map(m => m
+                .Throughout<Dummy>()
+                .Substitute
+                .This<int>(d => d.Int_1_ParameterLess)
+                .With<ReplacementObj>()
+                .This<int>(r => r.Int_2_ParameterLess))
+                .Save();
+
+            var dummy =
+                new Dummy();
+
+            var proxy =
+                container.Invoke<Dummy>();
+
+            var dummyRes =
+                dummy.Int_1_ParameterLess();
+            var proxyRes =
+                proxy.Int_1_ParameterLess();
+
+            Assert.AreEqual("simple", dummy.Text);
+            Assert.IsNullOrEmpty(proxy.Text);
+            Assert.AreNotEqual(dummyRes, proxyRes);
+            Assert.AreEqual(2, proxyRes);
         }
 
+        [Test]
         public void PassingBaseParameters()
         {
-            throw new NotImplementedException();
+            var container = new SurrogatesContainer();
+
+            container.Map(m =>
+                m.Throughout<Dummy>()
+                .Substitute.This<string, DateTime, Dummy.EvenMore, int>(d => d.Int_1_VariousParameters)
+                .With<ReplacementObj>()
+                .This<string, Dummy, DateTime, string, Dummy.EvenMore, int>(r => r.Int_2_VariousParametersPlusIntanceAndMethodName));
+
+            var dummy =
+                new Dummy();
+
+            var proxy =
+                container.Invoke<Dummy>();
+
+            // just to show that the rest of the object behaves as expected
+            dummy.Void_ParameterLess();
+            proxy.Void_ParameterLess();
+
+            Assert.IsNotNullOrEmpty(dummy.Text);
+            Assert.AreEqual("simple", dummy.Text);
+            Assert.IsNotNullOrEmpty(proxy.Text);
+            Assert.AreEqual("simple", proxy.Text);
+
+            //and now, the comparison between the two methods
+            var dummyRes =
+                dummy.Int_1_VariousParameters("this call was not made by the original method", DateTime.Now, new Dummy.EvenMore());
+
+            var proxyRes =
+                proxy.Int_1_VariousParameters("this call was not made by the original method", DateTime.Now, new Dummy.EvenMore());
+
+            Assert.IsNotNullOrEmpty(dummy.Text);
+            Assert.AreEqual("complex", dummy.Text);
+            Assert.IsNotNullOrEmpty(proxy.Text);
+            Assert.AreEqual("simple, this call was not made by the original method - method: Int_1_VariousParameters", proxy.Text);
+            Assert.AreNotEqual(dummyRes, proxyRes);
+            Assert.AreEqual(2, proxyRes);
         }
 
+        [Test, ExpectedException(typeof(NullReferenceException))]
         public void NotPassingBaseParameters()
         {
-            throw new NotImplementedException();
+            var container = new SurrogatesContainer();
+
+            container.Map(m =>
+                m.Throughout<Dummy>()
+                .Substitute.This<string, DateTime, Dummy.EvenMore, int>(d => d.Int_1_VariousParameters)
+                .With<ReplacementObj>()
+                .This<string, Dummy, DateTime, string, Dummy.EvenMore, int>(r => r.Int_2_VariousParametersWithDifferentNames))
+                .Save();
+
+            var dummy =
+                new Dummy();
+
+            var proxy =
+                container.Invoke<Dummy>();
+
+            dummy.Int_1_VariousParameters("text", DateTime.Now, new Dummy.EvenMore());
+            proxy.Int_1_VariousParameters("text", DateTime.Now, new Dummy.EvenMore());
         }
 
+        [Test]
         public void PassingInstanceAndMethodName()
         {
-            throw new NotImplementedException();
+            var container = new SurrogatesContainer();
+
+            container.Map(m => m
+                .Throughout<Dummy>()
+                .Substitute
+                .This<int>(d => d.Int_1_ParameterLess)
+                .With<ReplacementObj>()
+                .This<Dummy, string, int>(r => r.Int_2_InstanceAndMethodName));
+
+            var dummy =
+                new Dummy();
+
+            var proxy =
+                container.Invoke<Dummy>();
+
+            var dummyRes =
+                dummy.Int_1_ParameterLess();
+            var proxyRes =
+                proxy.Int_1_ParameterLess();
+
+            Assert.IsNotNullOrEmpty(dummy.Text);
+            Assert.AreEqual("simple", dummy.Text);
+
+            Assert.IsNotNullOrEmpty(proxy.Text);
+            Assert.AreEqual(typeof(Dummy).Name + "Proxy+Int_1_ParameterLess", proxy.Text);
+            Assert.AreNotEqual(dummyRes, proxyRes);
+            Assert.AreEqual(2, proxyRes);
         }
     }
 }
